2c4b536ec2c2bccb235b96c9e39c167cf88d8194
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-19 19:52:19 +0530

    update

--- models/route.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const routeSchema = new mongoose.Schema({
-  route_id: { type: Number, unique: true },
-  name: String
-});
-
-module.exports = mongoose.models.Route || mongoose.model('Route', routeSchema);
