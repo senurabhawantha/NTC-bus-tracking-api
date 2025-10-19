81448d2a862dbe909c896f1b9e7a59d89f0c06ba
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-19 15:26:02 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -12,14 +12,14 @@ const adminSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-adminSchema.pre('save', async function(next) {
+adminSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-adminSchema.methods.comparePassword = function(candidate) {
+adminSchema.methods.comparePassword = function (candidate) {
   return bcrypt.compare(candidate, this.password);
 };
 
