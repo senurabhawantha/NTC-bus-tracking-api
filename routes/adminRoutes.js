9262b83a4ecfa5d29c47e21540bfacb7e8bdac39
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-19 15:29:56 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const protect = require('../middleware/auth');
 const { createRoute, createBus } = require('../controllers/adminController');
-
 const router = express.Router();
 
 router.post('/routes', protect, createRoute);
